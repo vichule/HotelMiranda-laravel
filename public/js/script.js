5ebfa732a037afe97d198903041d577d49e3142b
vichule <javier.cabanasdiaz@gmail.com>
2024-05-20 17:24:24 +0200

    routes & scripts added

---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+const buttonMenu = document.querySelector('.header__button')
+const crossMenu = document.querySelector('#menuOpened')
+const barsMenu = document.querySelector('#menuClosed')
+const hiddenMenu = document.querySelector('.header__mobile-menu')
+const headerBar = document.querySelector('header')
+const form = document.querySelector('.details-container__form');
+
+let dropdown = false;
+
+buttonMenu.addEventListener('click', function () {
+
+    if (!dropdown) {
+
+        hiddenMenu.classList.remove('hidden');
+        crossMenu.classList.remove('hidden');
+        barsMenu.classList.add('hidden');
+        dropdown = true;
+    } else {
+        hiddenMenu.classList.add('hidden');
+        crossMenu.classList.add('hidden');
+        barsMenu.classList.remove('hidden');
+        dropdown = false;
+    }
+});
+
+window.addEventListener('scroll', function () {
+
+    let body = document.documentElement;
+
+    let scrollT = body.scrollTop || document.body.scrollTop;
+    let scrollH = body.scrollHeight || document.body.scrollHeight;
+
+
+    document.addEventListener("mousemove", (event) => {
+        let y = event.clientY
+
+
+
+        if (scrollT > 200 && y >= 150) {
+            headerBar.classList.remove('header-down')
+            headerBar.classList.add('header-normal')
+
+        } else if (scrollT < 200) {
+
+            headerBar.classList.remove('header-normal')
+            headerBar.classList.add('header-down')
+        }
+    });
+
+})
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/offers', function () {
+    return view('offers');
+});
+
+Route::get('/rooms', [RoomController::class, 'index']);
+
+Route::get('/rooms_details/{room}', [RoomController::class, 'show']);
+
+Route::get('/contact', [ContactController::class, 'store']);
