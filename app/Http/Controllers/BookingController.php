b984e3a38535c238544385c0dbab9238e2917044
vichule <javier.cabanasdiaz@gmail.com>
2024-05-30 16:04:37 +0200

    booking model/controller fixed and done

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -15,10 +17,33 @@ class BookingController extends Controller
         $request->validate([
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'check_in' => 'required|date|after:date',
+            'check_in' => 'required|date|after:today',
             'check_out' => 'required|date|after:check_in',
-            'notes' => 'required|string|max:255',
-            'room_id' => 'required|integer'
+            'notes' => 'nullable|string|max:255',
+            'room_id' => 'required|integer|exists:rooms,id'
         ]);
+
+        $checkIn = $request->check_in;
+        $checkOut = $request->check_out;
+        $roomId = $request->room_id;
+
+        $availableRooms = Room::roomAvailability($checkIn, $checkOut);
+        $isAvailable = $availableRooms->contains('id', $roomId);
+
+        if (!$isAvailable) {
+            return redirect()->back()->with('error', 'The room is not available for the selected dates.');
+        }
+
+        Booking::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'order_date' => now(),
+            'check_in' => $checkIn,
+            'check_out' => $checkOut,
+            'notes' => $request->notes,
+            'room_id' => $roomId
+        ]);
+
+        return redirect()->back()->with('success', 'Booking done successfully.');
     }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Booking extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 
     protected $fillable = [
         'first_name',
